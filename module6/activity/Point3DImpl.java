e8f06841a6a4f85687aface36a10af2ea3fdd3fb
AlexandrSpirin <42524956+AlexandrSpirin@users.noreply.github.com>
2018-12-19 15:36:47 +0300

    Update Point3DImpl.java

--- module6/activity/Point3DImpl.java
+++ module6/activity/Point3DImpl.java
@@ -16,7 +16,7 @@ public class Point3DImpl extends Point2DImpl implements Point3D{
 	 *
 	 */
 	public Point3DImpl(){
-		
+		move(0,0,0);
 	}
 	
 	/**
@@ -27,7 +27,7 @@ public class Point3DImpl extends Point2DImpl implements Point3D{
 	 * @param z coordinate along the z axis
 	 */
 	public Point3DImpl(double x, double y, double z){
-		
+		move(x,y,z);
 	}
 	
 	
@@ -35,7 +35,7 @@ public class Point3DImpl extends Point2DImpl implements Point3D{
 	 * @see sef.module5.activity.Point3D#setZ(double)
 	 */
 	public final void setZ(double z){
-	
+		this.z=z;
 	}
 	
 	
@@ -43,14 +43,15 @@ public class Point3DImpl extends Point2DImpl implements Point3D{
 	 * @see sef.module5.activity.Point3D#getZ()
 	 */
 	public final double getZ(){
-		return 0;
+		return this.z;
 	}
 	
 	/* (non-Javadoc)
 	 * @see sef.module5.activity.Point3D#move(double, double, double)
 	 */
 	public void move(double x2, double y2, double z2){
-	
+		move(x2,y2);
+		setZ(z2);
 	}
 	
 	
@@ -58,7 +59,8 @@ public class Point3DImpl extends Point2DImpl implements Point3D{
 	 * @see sef.module5.activity.Point3D#translate(double, double, double)
 	 */
 	public void translate(double x2, double y2, double z2){
-		
+		translate(x2,y2);
+		setZ(this.z+z2);
 	}
 	
 	
@@ -66,8 +68,8 @@ public class Point3DImpl extends Point2DImpl implements Point3D{
 	 * @see sef.module5.activity.Point3D#equals(double, double, double)
 	 */
 	public boolean equals(double x2, double y2, double z2){
-		
-		return false;
+
+		return z2==this.z && equals(x2,y2);
 	}
 	
 	
@@ -80,7 +82,7 @@ public class Point3DImpl extends Point2DImpl implements Point3D{
 	 */
 	public boolean equals(Object p){
 	
-		return false;
+		return this == p;
 		
 	}
 	
@@ -90,7 +92,7 @@ public class Point3DImpl extends Point2DImpl implements Point3D{
 	 */
 	public double getDistance(Point3D p){
 	
-		return 0;
+		return Math.sqrt(Math.pow((p.getX()-getX()),2)+Math.pow((p.getY()-getY()),2)+Math.pow((p.getZ()-getZ()),2));
 	}
 	
 
@@ -99,7 +101,7 @@ public class Point3DImpl extends Point2DImpl implements Point3D{
 	 */
 	public double getDistance(double x2, double y2, double z2){
 		
-		return 0;
+		return Math.sqrt(Math.pow((x2-getX()),2)+Math.pow((y2-getY()),2)+Math.pow((z2-getZ()),2));
 	
 	}
 	
